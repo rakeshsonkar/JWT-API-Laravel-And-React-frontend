f3f955c003ad9de4df6a95e1667a88f82b97f36b
rakeshsonkar <rksonkar356@gmail.com>
2022-04-25 08:00:16 +0530

    secound

---
+++ APIJWT/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api'
+], function ($router) {
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/refresh', [UserController::class, 'refresh']);
+    Route::get('/user-profile', [UserController::class, 'userProfile']);
+    Route::post('/update-Profile', [UserController::class, 'updateProfile']);
+});
+
